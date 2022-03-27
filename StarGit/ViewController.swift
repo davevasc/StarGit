54b5796f69d894c5af435e338e7eff29488964c9
dave <daveplus@gmail.com>
2022-03-27 01:36:41 +0100

    Initial Commit

---
+++ StarGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarGit
+//
+//  Created by David Vicente on 27/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
